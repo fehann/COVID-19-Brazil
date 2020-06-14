cf18034391490d0a66c161453654c87431a80b1c
fehann <fhannaka@hotmail.com>
2020-06-14 13:04:10 -0300

    testing

---
+++ test.py
@@ -0,0 +1,22 @@
+# Import packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import datadotworld as dw
+
+# Set style & figures inline
+sns.set()
+# %matplotlib inline
+
+st.write("""
+# Testing
+Does this work?
+""")
+
+# Importando dados do site
+#data = pd.read_csv('https://brasil.io/dataset/covid19/caso?format=csv')
+data = dw.load_dataset('fehann/brasil-io-covid19')
+
+st.dataframe(data)
